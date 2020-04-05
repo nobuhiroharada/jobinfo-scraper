d751c0ed740b38212c065eb10cd228bfe7665466
nobuhiroharada <nobuhiroharada@hotmail.com>
2020-04-05 16:59:23 +0900

    Crowdtech追加
    微修正

--- settings.py
+++ settings.py
@@ -8,3 +8,25 @@ dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
 KEY = os.environ['ENV_KEY']
+
+total_projects = ['total_projects']
+
+target_programs_frameworks = [
+                                'php', 'laravel', 'cakephp', 'wordpress',
+                                'ruby', 'rails',
+                                'python', 'django', 'flask',
+                                'java', 'spring', 'struts', 'kotlin',
+                                'javascript', 'react', 'vue', 'jquery',
+                                'swift', 'objective-c',
+                                'go',
+                                'gcp', 'aws', 'azure',
+                                'linux',
+                                'git', 'svn',
+                                'jenkins', 'circle',
+                                'mysql', 'postgresql', 'oracle', 'sql server', 
+                                'sql', 
+                                
+                            ]
+
+url_levtech = 'https://freelance.levtech.jp/project/search/'
+url_crowdtech = 'https://crowdtech.jp/job_offers'
--- target_cites/crowdtech.py
+++ target_cites/crowdtech.py
@@ -20,7 +20,7 @@ functions = Functions()
 
 exec_date = functions.get_exec_date()
 
-target_name = 'levtech'
+target_name = 'crowdtech'
 
 total_projects = settings.total_projects
 total_projects_results = []
@@ -31,12 +31,12 @@ print('start scraping : ' + target_name + ' ' + exec_date)
 print('open browser : ' + target_name)
 
 driver = webdriver.Chrome(chromedriver)
-driver.get(settings.url_levtech)
+driver.get(settings.url_crowdtech)
 
 time.sleep(1)
 
 # 総案件数
-total_projects_results.append(driver.find_element_by_xpath('/html/body/div[2]/div[2]/div/div[2]/div[1]/div/p/span').text)
+total_projects_results.append(driver.find_element_by_xpath('/html/body/main/div[5]/div/div[1]/div[1]/span[2]').text)
 
 # 総案件数 CSVに出力
 with open(donwload_dir + '/' + target_name + '_total_projects_' + exec_date + '.csv', 'w') as f:
@@ -47,12 +47,12 @@ with open(donwload_dir + '/' + target_name + '_total_projects_' + exec_date + '.
 
 # 検索ワード毎の案件数取得
 for w in search_words:
-    search_input = driver.find_element_by_xpath('//*[@id="activeCount"]/div[1]/div[1]/div/input')
+    search_input = driver.find_element_by_xpath('/html/body/main/div[4]/form/div[1]/div[4]/div[1]/div[6]/div/input')
     search_input.clear()
     search_input.send_keys(w)
     search_input.send_keys(Keys.ENTER)
-    time.sleep(1)
-    search_results.append(driver.find_element_by_xpath('//*[@id="activeCount"]/div[2]/div/p/span').text)
+    time.sleep(3)
+    search_results.append(driver.find_element_by_xpath('/html/body/main/div[5]/div/div[1]/div[1]/span[2]').text)
 
 # 検索ワード毎の案件数取得 CSVに出力
 with open(donwload_dir + '/' + target_name + '_search_words_' + exec_date + '.csv', 'w') as f:
